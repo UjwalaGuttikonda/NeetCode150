998195a7b1a3320563789275df2ce4c1b4b42d9a
UjwalaGuttikonda <ujwalaguttikonda@gmail.com>
2023-12-23 15:42:33 -0500

    Group anagram

---
+++ Arrays/Group_Anagram_Sorting_O(nlogK).java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs == null || strs.length ==0)
+            return new ArrayList<>();
+        
+        Map<String, List<String>> anagramMap = new HashMap<>();
+        for(String s:strs)
+        {
+            char [] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+        
+
+            if(!anagramMap.containsKey(key))
+                anagramMap.put(key, new ArrayList<>());
+            anagramMap.get(key).add(s);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for(Map.Entry<String, List<String>> anagrams : anagramMap.entrySet()){
+            result.add(anagrams.getValue());
+        }
+        return result;
+
+    }
+}
